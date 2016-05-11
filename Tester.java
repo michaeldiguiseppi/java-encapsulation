76501067f7dbdfdb6ef29453ebec661510abdb8b
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-11 16:28:27 -0600

    Added solutions

--- Tester.java
+++ Tester.java
@@ -3,10 +3,10 @@ public class Tester {
   public static void main(String[] args) {
 
     Time time = new Time();
-    time.hour = 12;
-    time.minute = 20;
-    time.second = 50;
+    time.setHour(12);
+    time.setMinute(20);
+    time.setSecond(50);
     time.displayTime();
   }
-  
+
 }
--- Time.java
+++ Time.java
@@ -1,12 +1,36 @@
 public class Time {
 
   //make these private
-  int hour;
-  int minute;
-  int second;
+  private int hour;
+  private int minute;
+  private int second;
 
   public void displayTime() {
     System.out.println("The time is: " + hour + ":" + minute + ":" + second);
+  }
+
+  public int getHour() {
+    return this.hour;
+  }
+
+  public void setHour(int h) {
+    this.hour = h;
+  }
+
+  public int getMinute() {
+    return this.minute;
+  }
+
+  public void setMinute(int m) {
+    this.minute = m;
+  }
+
+  public int getSecond() {
+    return this.second;
+  }
+
+  public void setSecond(int s) {
+    this.second = s;
   }
 
 }
